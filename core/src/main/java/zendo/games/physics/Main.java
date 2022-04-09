4c1aa38c7348db7b88bb97fbf8f715644e649fe3
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2022-04-09 18:27:42 -0500

    Start aggregating 3d/collision components together into a GameObject

--- core/src/main/java/zendo/games/physics/Main.java
+++ core/src/main/java/zendo/games/physics/Main.java
@@ -9,12 +9,16 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.Bullet;
 import com.badlogic.gdx.physics.bullet.BulletBase;
 import com.badlogic.gdx.physics.bullet.collision.*;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ArrayMap;
+import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.ScreenUtils;
+import lombok.RequiredArgsConstructor;
 
 import static com.badlogic.gdx.Input.Keys;
 import static com.badlogic.gdx.graphics.VertexAttributes.Usage;
@@ -33,6 +37,12 @@ public class Main extends ApplicationAdapter {
 	btDispatcher dispatcher;
 	btCollisionConfiguration collisionConfig;
 
+	Array<GameObject> gameObjects = new Array<>();
+
+	// ------------------------------------------------------------------------
+	// Data structures
+	// ------------------------------------------------------------------------
+
 	public static class Config {
 		public static final int fov = 67;
 		public static final int width = 1280;
@@ -45,23 +55,56 @@ public class Main extends ApplicationAdapter {
 		public static Model ground;
 	}
 
-	public static class Instances {
-		public static final Array<ModelInstance> all = new Array<>();
-		public static ModelInstance ball;
-		public static ModelInstance ground;
-	}
-
 	public static class CollisionShapes {
 		public static final Array<btCollisionShape> all = new Array<>();
 		public static btCollisionShape ball;
 		public static btCollisionShape ground;
 	}
 
-	public static class CollisionObjects {
-		public static final Array<btCollisionObject> all = new Array<>();
-		public static btCollisionObject ball;
-		public static btCollisionObject ground;
+	// TODO - should the entire scene be a single model and the various components just be nodes in that model?
+
+	public static class GameObject extends ModelInstance implements Disposable {
+
+		public final btCollisionObject body;
+
+		public boolean moving;
+
+		public GameObject(Model model, btCollisionShape shape) {
+			super(model);
+			body = new btCollisionObject();
+			body.setCollisionShape(shape);
+		}
+
+		@Override
+		public void dispose() {
+			body.dispose();
+		}
+
+		@RequiredArgsConstructor
+		public static class Builder implements Disposable {
+
+			public final Model model;
+			public final btCollisionShape shape;
+
+			public GameObject build() {
+				return new GameObject(model, shape);
+			}
+
+			@Override
+			public void dispose() {
+				shape.dispose();
+			}
+
+		}
+
 	}
+
+	final ArrayMap<String, GameObject.Builder> builders = new ArrayMap<>();
+
+
+	// ------------------------------------------------------------------------
+	// Methods
+	// ------------------------------------------------------------------------
 
 	@Override
 	public void create() {
@@ -101,33 +144,31 @@ public class Main extends ApplicationAdapter {
 		Models.ground = builder.createBox(5f, 1f, 5f, groundMaterial, attribs);
 		Models.all.addAll(Models.ball, Models.ground);
 
-		// model instances
-		Instances.ball = new ModelInstance(Models.ball);
-		Instances.ground = new ModelInstance(Models.ground);
-		Instances.all.addAll(Instances.ball, Instances.ground);
-
-		Instances.ball.transform.setToTranslation(0f, 9f, 0f);
-
 		// collision shapes
 		CollisionShapes.ball = new btSphereShape(0.5f);
 		CollisionShapes.ground = new btBoxShape(new Vector3(2.5f, 0.5f, 2.5f));
 
 		CollisionShapes.all.addAll(CollisionShapes.ball, CollisionShapes.ground);
 
-		// collision objects
-		CollisionObjects.ball = new btCollisionObject();
-		CollisionObjects.ground = new btCollisionObject();
+		// populate game object builders
+		builders.put("ball", new GameObject.Builder(Models.ball, CollisionShapes.ball));
+		builders.put("ground", new GameObject.Builder(Models.ground, CollisionShapes.ground));
 
-		CollisionObjects.all.addAll(CollisionObjects.ball, CollisionObjects.ground);
+		// create game objects
+		// NOTE: important to add the ground first, as we'll be referencing it by gameObjects.first() later
+		var ground = builders.get("ground").build();
+		gameObjects.add(ground);
 
-		CollisionObjects.ball.setCollisionShape(CollisionShapes.ball);
-		CollisionObjects.ball.setWorldTransform(Instances.ball.transform);
-
-		CollisionObjects.ground.setCollisionShape(CollisionShapes.ground);
-		CollisionObjects.ground.setWorldTransform(Instances.ground.transform);
+		int numBalls = 5;
+		for (int i = 0; i < numBalls; i++) {
+			var ball = builders.get("ball").build();
+			ball.moving = true;
+			ball.transform.setToTranslation(MathUtils.random(-2.5f, 2.5f), MathUtils.random(6f, 10f), MathUtils.random(-2.5f, 2.5f));
+			ball.body.setWorldTransform(ball.transform);
+			gameObjects.add(ball);
+		}
 	}
 
-	boolean collision = false;
 	public void update() {
 		var delta = Math.min(1f / 30f, Gdx.graphics.getDeltaTime());
 
@@ -135,10 +176,16 @@ public class Main extends ApplicationAdapter {
 			Gdx.app.exit();
 		}
 
-		if (!collision) {
-			Instances.ball.transform.translate(0f, -delta, 0f);
-			CollisionObjects.ball.setWorldTransform(Instances.ball.transform);
-			collision = checkCollision(CollisionObjects.ball, CollisionObjects.ground);
+		final float speed = 9.8f;
+		for (var obj : gameObjects) {
+			if (obj.moving) {
+				obj.transform.trn(0f, -speed * delta, 0f);
+				obj.body.setWorldTransform(obj.transform);
+				var ground = gameObjects.first();
+				if (checkCollision(obj.body, ground.body)) {
+					obj.moving = false;
+				}
+			}
 		}
 
 		camController.update();
@@ -178,7 +225,7 @@ public class Main extends ApplicationAdapter {
 		ScreenUtils.clear(Color.SKY, true);
 
 		batch.begin(camera);
-		batch.render(Instances.all, env);
+		batch.render(gameObjects, env);
 		batch.end();
 	}
 
@@ -186,10 +233,12 @@ public class Main extends ApplicationAdapter {
 	public void dispose() {
 		Models.all.forEach(Model::dispose);
 		CollisionShapes.all.forEach(BulletBase::dispose);
-		CollisionObjects.all.forEach(BulletBase::dispose);
 		collisionConfig.dispose();
 		dispatcher.dispose();
 		batch.dispose();
+
+		gameObjects.forEach(GameObject::dispose);
+		builders.values().forEach(GameObject.Builder::dispose);
 	}
 
 }
